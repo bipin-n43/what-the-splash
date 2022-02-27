da5306b0d6d515d9e4b5f054de9b1c864e0489de
bipinnepal43 <bipinnepal43@gmail.com>
2022-02-27 22:06:43 +0545

    first commit

--- src/sagas/imagesSaga.js
+++ src/sagas/imagesSaga.js
@@ -5,6 +5,7 @@ import {setImages,setError} from "../actions ";
 const getPage = state => state.nextPage()
 function* handleImagesLoad(){
     try {
+
         const page = yield select(getPage)
         const images = yield call(fetchImages,page)
         yield put (setImages(images))
